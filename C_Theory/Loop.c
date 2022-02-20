7fa4bb10ec9e254e0440184191413ca22da4346b
DongYoon <canfrog@naver.com>
2022-02-20 11:45:19 +0900

    Learn Loop

---
+++ C_Theory/Loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int a = 10;
+	int b = 3;
+
+	while(a > b) {
+		a--;
+		printf("a : %d\n", a);
+	}
+	
+	int p;	
+	for (p = 0; p < 10; p++) {
+		printf("나는 사랑스런 하나님의 자녀이다 %d\n", p);
+	}
+	
+
+	return 0;
+}
